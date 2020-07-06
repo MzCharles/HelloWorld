3fb136bffdced550f709906af781ac018ec8b11c
MzCharles <47841386+MzCharles@users.noreply.github.com>
2020-07-06 15:15:09 -0400

    added v2 in ln.

--- src/ca/concordia/cce/cejv416/exercises/Main.java
+++ src/ca/concordia/cce/cejv416/exercises/Main.java
@@ -3,6 +3,6 @@ package ca.concordia.cce.cejv416.exercises;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello Github test");
+	System.out.println("hello Github test v2");
     }
 }
